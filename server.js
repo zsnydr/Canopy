50438df33aa5813cf202bda3a4aba10c63ced50b
Seungkyu Choi <VictorChoi@Seungkyus-MacBook-Pro.local>
2016-11-02 18:15:54 -0700

    adding basic Express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'bundleSrc')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(app.get('port'));
+console.log('Listening to port... ', app.get('port'));
