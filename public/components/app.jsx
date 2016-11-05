4b6dcf1cf0486a6d022f485ec5a7e5b834b4e9d8
Sean Enright <seanenright@Seans-MacBook-Pro.local>
2016-11-05 16:46:09 -0700

    [add] splash page image, search form, update dev dependencies Sass

--- public/components/app.jsx
+++ public/components/app.jsx
@@ -5,12 +5,17 @@ import { createStore } from 'redux';
 import RentList from '../containers/rent-list';
 import reducers from '../reducers/index';
 
+const SplashPage = require('./splash_page').default;
+
+
+
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <RentList />
       </Provider>
+       
     );
   }
 }
---
+++ public/components/splash_page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <form>
+            <input>
+              
+            </input>
+          </form>
+
+        </div>
+        <img class='backgroundImage' src='http://s7d4.scene7.com/is/image/roomandboard/hutton_706468_16e_g?$str_g$&size=1520,960&scl=1' alt='splash' />
+
+      </div>
+    );
+  }
+}
