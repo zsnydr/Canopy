3e1ea5cb5f235709ec2849301968658fa2f33631
zsnydr <zackwsnyder@gmail.com>
2016-11-08 18:44:01 -0800

    [add] add City model to DB, refactor getListings dbHelper

--- dbHelpers.js
+++ dbHelpers.js
@@ -1,18 +1,20 @@
 const Listing = require('./db/schema').Listing;
+const City = require('./db/schema').City;
 // const Host = require('./db/schema').Host;
 // const Renter = require('./db/schema').Renter;
 // const RenterListing = require('./db/schema').RenterListing;
 
 module.exports = {
   getListings: (city) => {
-    return Listing.findAll({
+    return City.findOrCreate({
       where: {
-        city: city.slice(0, -4).toUpperCase(),
+        name: city.slice(0, -4).toUpperCase(),
         state: city.slice(-2).toUpperCase()
-      }
+      },
+      include: [Listing]
     })
-    .then((listings) => {
-      return listings;
+    .spread((cityData, created) => {
+      return created ? {} : cityData;
     })
     .catch((err) => {
       return `Error getting listings: ${err}`;
