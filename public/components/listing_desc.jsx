bb8f50c0b62cc1e67ec8ba289723a1645ff9a8fd
Sean Enright <seanenright86@gmail.com>
2016-11-08 19:29:39 -0800

    [add] mvp client structure complete

--- public/components/listing_desc.jsx
+++ public/components/listing_desc.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ListingDesc = (props) => {
+const ListingDesc = ({ activeListing, listingData }) => {
   return (
     <div>
-      <h3>{props.street}</h3>
-      <h4>`${props.city}, ${props.state} ${props.zip}`</h4><br />
+      <h3>{activeListing.street}</h3>
+      <h4>{listingData.name}, {listingData.state} {activeListing.zip}</h4><br />
       <div>
-        Unit: {props.unit}
-        Beds: {props.beds}
-        Baths: {props.baths}
-        Rent: {props.rent}
-        SqFoot: {props.sqFoot}
-        Available: {props.availableDate}
+        Unit: {activeListing.unit}
+        Beds: {activeListing.beds}
+        Baths: {activeListing.baths}
+        Rent: {activeListing.rent}
+        SqFoot: {activeListing.sqFoot}
+        Available: {activeListing.availableDate}
         <br />
         <button>Apply</button>
       </div>
