5fa4e3731d42ae6fc21e1e91c37e2e135cb1c161
Sean Enright <seanenright86@gmail.com>
2016-11-09 20:16:09 -0800

    [styles] map added to listing page and scss

--- public/containers/listing-page.jsx
+++ public/containers/listing-page.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GoogleMaps from '../components/google_maps';
+
 
 import ListingDetail from '../components/listing_details';
 
@@ -15,8 +17,10 @@ class ListingPage extends Component {
     }
 
     return (
-      <div>
-        Google Map
+      <div className='listing_page'>
+        <div className='listingMap'>
+          <GoogleMaps listings={[this.props.activeListing]} />
+        </div>
         <ListingDetail activeListing={this.props.activeListing} listingData={this.props.listingData} />
       </div>
     );
