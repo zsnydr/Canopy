f38f7000da9ae315149ec8a889585197b703e4cb
Seungkyu Choi <VictorChoi@Seungkyus-MacBook-Pro.local>
2016-11-10 00:45:35 -0800

    can change photos by clicking

--- public/components/listing_pics.jsx
+++ public/components/listing_pics.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ListingPics = (props) => {
-  return (
-    <div>
-      {props.images.map((image) => (
-        <img key={image} src={__dirname+"assets/"+image} />
-      ))}
-    </div>
-  );
-};
+import CurrentPicture from './listing_pic_current';
+
+class ListingPics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImage: 'assets/'+this.props.images[0]
+    };
+    this.updateCurrentImage = this.updateCurrentImage.bind(this);
+  }
+
+  updateCurrentImage(event) {
+    console.log(event.target, event.target.path);
+    console.log('target src: ',event.target.src.slice(22));
+    this.setState({
+      currentImage: event.target.src.slice(22)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <CurrentPicture picture={this.state.currentImage} />
+        {this.props.images.map((image) => (
+          <img
+            className="smallPics"
+            key={image}
+            src={__dirname+"assets/"+image}
+            onClick={this.updateCurrentImage} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ListingPics;
+
