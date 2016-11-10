b0a38d9eb98ad1494b7ee78d4205237040130dc5
Seungkyu Choi <VictorChoi@Seungkyus-MacBook-Pro.local>
2016-11-09 17:07:38 -0800

    [fix] style of google map

--- public/components/google_maps.jsx
+++ public/components/google_maps.jsx
@@ -17,7 +17,7 @@ export default (props) => {
   };
   return (
     <GoogleMapLoader
-      containerElement={<div style={{ height: '400px' }} />}
+      containerElement={<div style={{ height: '100%' }} />}
       googleMapElement={
         <GoogleMap 
           defaultZoom={12} 
