482453dbab3e20a15a419fd2e9cd974a7103c845
Sean Enright <seanenright86@gmail.com>
2016-11-16 13:52:21 -0800

    [fix] listing page add photos and address back to normal

--- dbHelpers.js
+++ dbHelpers.js
@@ -115,7 +115,6 @@ module.exports = {
       .catch((err) => {
         return `Error posting listing: ${err}`;
       })
-    .catch(err=>{console.err(err)});
   },
 
   postImages: (imageData) => {
