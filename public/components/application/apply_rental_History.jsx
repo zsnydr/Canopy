225cfd7d62458ae08869afeffc7abdc3cb177e3a
Sean Enright <seanenright86@gmail.com>
2016-11-23 19:33:53 -0800

    [fix] application form functionality fixed

--- public/components/application/apply_rental_History.jsx
+++ public/components/application/apply_rental_History.jsx
@@ -16,7 +16,8 @@ class RentalHistoryForm extends Component {
       landlordPhone: 2131,
       rentalPayment: 2,
       reasonLeft: 'Victor Choi',
-      application_id: 1
+      application_id: 1,
+      show: true
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -26,10 +27,10 @@ class RentalHistoryForm extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    this.props.subRentalHistory();
     request.post('/api/rentalHistory', this.state)
     .then((rentalHistory) => {
       console.log('Successfullly stored this rental history data to DB: ', rentalHistory);
+      this.setState({show: false});
     });
   }
 
@@ -54,28 +55,37 @@ class RentalHistoryForm extends Component {
       const state = {};
       state[key] = e.target.value;
       this.setState(state);
+      console.log (this.state);
     };
   }
 
   render() {
+    if (this.state.show) {
+      return (
+        <div className="listingForm">
+          <h3>Rental History</h3>
+          <br />
+          <Form inline>
+            <FormText type="city" handleChange={this.handleChange} placeholder=" eg.Chicago" />
+            <FormText type="street" handleChange={this.handleChange} placeholder=" eg.1060 W. Addison" />
+            <FormText type="zip" handleChange={this.handleChange} placeholder=" eg.88888" />
+            <br />
+            <FormNumber type="rentalPayment" handleChange={this.handleChange} placeholder="1500" />
+            <FormText type="landlordName" handleChange={this.handleChange} placeholder="Josh" />
+            <FormNumber type="landlordPhone" handleChange={this.handleChange} placeholder="4088675309" />
+            <br />
+            <FormText type="reasonLeft" handleChange={this.handleChange} placeholder="big fight" />
+            <br />
+            <Button onClick={this.onFormSubmit} type="submit">
+              Submit rental history
+            </Button>
+          </Form>
+        </div>
+      );
+    }
+
     return (
-      <div className="listingForm">
-        <Form inline>
-          <FormText type="city" handleChange={this.handleChange} placeholder=" eg.Chicago" />
-          <FormText type="street" handleChange={this.handleChange} placeholder=" eg.1060 W. Addison" />
-          <FormText type="zip" handleChange={this.handleChange} placeholder=" eg.88888" />
-          <br />
-          <FormNumber type="rentalPayment" handleChange={this.handleChange} placeholder="88888" />
-          <FormText type="landlordName" handleChange={this.handleChange} placeholder="Josh" />
-          <FormNumber type="landlordPhone" handleChange={this.handleChange} placeholder="1231231212" />
-          <br />
-          <FormText type="reasonLeft" handleChange={this.handleChange} placeholder="big fight" />
-          <br />
-          <Button onClick={this.onFormSubmit} type="submit">
-            Submit rental history
-          </Button>
-        </Form>
-      </div>
+      <div> success! </div>
     );
   }
 }
