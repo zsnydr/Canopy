eb5f17c4cbdc93f6a4f11dce30a702958f8d7a29
Seungkyu Choi <VictorChoi@Seungkyus-MacBook-Pro.local>
2016-11-26 17:53:25 -0800

    [fix] edit profile changing password when unexpeted, not changing the name and home city correctly, and not changing the active user correctlly, all fixed

--- public/components/profile/user_profile_info.jsx
+++ public/components/profile/user_profile_info.jsx
@@ -35,7 +35,7 @@ class UserProfileInfo extends Component {
         </div>
           { (this.props.activeUser.userType === 0) &&
             <div className="btn-group-vertical col-md-4">
-              <button type="button" class="btn btn-info" onClick={this.props.editUserInfo}>Edit User Profile</button> 
+              <button type="button" className="btn btn-info" onClick={this.props.editUserInfo}>Edit User Profile</button> 
               <button className="btn btn-info" onClick={this.goToSubmitApplication}>Edit Application</button>
               <button className="btn btn-info" onClick={this.goToViewApplication}>View Application</button>
             </div>
